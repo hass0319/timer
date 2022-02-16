0d2377d62540afa5e8c82f728c14e17fdb61f5ef
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-02-16 14:59:23 +0000

    fixed

--- timer1.js
+++ timer1.js
@@ -2,18 +2,19 @@ const time = process.argv.slice(2);
 
 const alarmClock = (time) => {
   if (time.length === 0) return 'insert numbers';
-  let sortedTime = time.sort((a,b) => a - b)
+  let sortedTime = time.sort((a,b) => a - b);
   sortedTime.forEach((timer, i, arr)=> {
     if (timer < 0) return 'timer negative';
-    if(timer === NaN) return 'insert number';
-    if (timer > 0) {
+    if (timer === isNaN()) return 'insert number';
+    if (timer >= 0) {
       setTimeout(() => {
         process.stdout.write('\x07');
         process.stdout.write('BLOW');
-        if (i === arr.lenght-1) {
-          process.stdout.write('\n')
-      };
-      }, timer * 1000)
+        if (i === arr.length - 1) {
+          process.stdout.write('\n');
+        }
+      }, timer * 1000);
     }
-  })
-}
+  });
+};
+alarmClock(time);
